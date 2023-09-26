eb34509c5892bc231af4551484f04b8103f9adc9
Dingo497 <banas.martin1181@gmail.com>
2023-09-26 13:16:18 +0200

    Pridanie react dev tools

--- resources/js/redux/store.ts
+++ resources/js/redux/store.ts
@@ -1,10 +1,14 @@
 import { createStore, combineReducers } from 'redux';
 import authReducer from './reducers/authReducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    composeWithDevTools()
+);
 
 export default store;
