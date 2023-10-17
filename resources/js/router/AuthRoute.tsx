d59839fdcc1aa08765c4fd85456af43317cedcee
Dingo497 <banas.martin1181@gmail.com>
2023-10-18 00:17:09 +0200

    Oprava: Ak sa nachadzam na vytvoreni/uprave dokumentu podstranke a dam refresh stranky tak po refreshi ak som prihlaseny tak dam redirect na /dashboard, kvoli tomu ze dane podstranky nemaju poriesenu logiku zapametania udajov po refreshi stranky

--- resources/js/router/AuthRoute.tsx
+++ resources/js/router/AuthRoute.tsx
@@ -1,11 +1,12 @@
 import {useDispatch, useSelector} from 'react-redux';
 import { AppStateTypes } from "../redux/constants/appStateTypes";
-import { Navigate} from 'react-router-dom';
+import { Navigate, useNavigate} from 'react-router-dom';
 import {useLayoutEffect, useState} from "react";
 import {getUserDocumentsAfterRefreshAction} from "../redux/actions/documentActions";
 
 export default function AuthRoute({ children }) {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const token = useSelector((state: AppStateTypes) => state.auth.token);
 
@@ -24,6 +25,11 @@ export default function AuthRoute({ children }) {
                     setIsDispatchComplete(true);
                 })
         } else {
+            // Ak som prihlaseny a spravim refresh a nachadzam sa na create-document alebo edit-document tak nech
+            // spravi redirect na dashboard, kvoli tomu ze tam nemam poriesene zapametanie udajov po refreshe stranky
+            if (children.type.name === 'CreateDocument' || children.type.name === 'EditDocument') {
+                return navigate('/dashbaord');
+            }
             setIsDispatchComplete(true);
         }
     }, [dispatch, token]);
